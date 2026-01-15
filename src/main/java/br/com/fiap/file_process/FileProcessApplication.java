f1a958e553976bc226cf5558106fe6048a663dcb
Lucas Machado <contato.lucasmmachado@gmail.com>
2026-01-15 20:43:12 -0300

    Project start

---
+++ src/main/java/br/com/fiap/file_process/FileProcessApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.file_process;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileProcessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileProcessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/file_process/FileProcessApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.file_process;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileProcessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
